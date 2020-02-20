a811641a85eb03650cc0f8f015bca3adefff14eb
Artem <uspenskiyart@gmail.com>
2020-02-20 20:44:27 +0300

    task is ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array===undefined || array.length ===0){
+    return 0;
+  }else {
+    function sorter(a, b) {
+      if (a > b) return 1;
+      if (a == b) return 0;
+      if (a < b) return -1;
+    }
+    let arrSort = array.sort(sorter);
+    return arrSort[0];
+  }
 }
+
+
 
 exports.max = function max (array) {
-  return 0;
+  if(array===undefined || array.length ===0){
+    return 0;
+  }else {
+    function sorter(a, b) {
+      if (a > b) return 1;
+      if (a == b) return 0;
+      if (a < b) return -1;
+  }
+    let arrSort2 = array.sort(sorter).reverse();
+    return arrSort2[0];
+ }
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if(array===undefined || array.length ===0){
+    return 0;
+  }else {
+    let leng = array.length;
+    let reducer = (res, value)=> res+value;
+    let sum = array.reduce(reducer);
+    let avg = sum/leng;
+    return avg;
+ }
 }
